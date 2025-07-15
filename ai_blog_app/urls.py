4ca59a6c3f6460c0cd99c6cd69761fda596a1dcd
Adnan khan <adnan076234@gmail.com>
2025-07-15 16:59:54 +0530

    configured urls.py

--- ai_blog_app/urls.py
+++ ai_blog_app/urls.py
@@ -18,11 +18,21 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from . import views
 
+# urlpatterns = [
+#     path('admin/', admin.site.urls),
+#     path('', include('blog_generator.urls'))
+# ]
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('blog_generator.urls'))
+    path('', views.index, name='index'),
+    path('generate/', views.generate_blog, name='generate_blog'),
+    path('blogs/', views.blog_list, name='blog-list'),
+    path('blog/<int:pk>/', views.blog_details, name='blog-details'),
+    path('login/', views.user_login, name='login'),
+    path('signup/', views.user_signup, name='signup'),
+    path('logout/', views.user_logout, name='logout'),
 ]
 
 urlpatterns = urlpatterns+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
