b135f2da7048204edc2d0ae3c97304699706bb19
Adnan khan <adnan076234@gmail.com>
2025-10-19 19:49:13 +0530

    Added the new database connection

--- blog_generator/views.py
+++ blog_generator/views.py
@@ -17,7 +17,7 @@ logger = logging.getLogger(__name__)
 GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
 
 genai.configure(api_key=GEMINI_API_KEY)
-model = genai.GenerativeModel('gemini-1.5-flash')
+model = genai.GenerativeModel('gemini-2.5-flash')
 
 
 @login_required
